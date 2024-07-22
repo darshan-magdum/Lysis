f228a5955f868a38cfeaa85749b0ccc0e01bec70
darshan.magdum <darshan.magdum@nihilent.com>
2024-07-22 12:45:55 +0530

    home page changes with navbar and image with styles

---
+++ Frontend/lysis/src/Components/HomeNavbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const HomeNavbar = () => {
+  return (
+    <header>
+      <div className="container">
+        <a href="#" className="logo">
+          <b>Lysis</b>
+        </a>
+        <ul className="links">
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Get Started</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default HomeNavbar;
--- Frontend/lysis/src/Screens/HomePage.js
+++ Frontend/lysis/src/Screens/HomePage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React from 'react';
+import '../Styles/HomePage.css'; 
+import HomeNavbar from '../Components/HomeNavbar';
+import CodeGirl from "../../src/Images/CodeGirl.png"
 
 const HomePage = () => {
   return (
-    <div className="homepage">
-      {/* Header content */}
-      <header className="homepage-header">
-        <h1>Lysis</h1>
-      </header>
-      <main>{/* Main content */}</main>
-      <footer className="homepage-footer">{/* Footer content */}</footer>
+    <div className="main-container">
+      <div className="blur-circle1"></div>
+      <div className="blur-circle2"></div>
+      <div className="landing-page">
+        <HomeNavbar/>
+        <div className="content">
+          <div className="container">
+            <div className="info">
+              <h1>Unveil Code Complexity</h1>
+              <p>
+              Enhance code understanding with AI-driven analysis and detailed documentation.
+ We Save time on manual documentation and code reviews.
+
+              </p>
+              <button>Explore More</button>
+            </div>
+            <div className="image">
+              <img
+                className="main-image"
+                src={CodeGirl}
+                alt="Coding Girl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
