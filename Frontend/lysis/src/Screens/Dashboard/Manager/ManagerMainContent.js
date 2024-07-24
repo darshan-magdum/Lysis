fed573fcfebf4c2c1c9fffa9cd78c6780c7fdf36
darshan.magdum <darshan.magdum@nihilent.com>
2024-07-24 08:40:16 +0530

    added iamge profile

--- Frontend/lysis/src/Screens/Dashboard/Manager/ManagerMainContent.js
+++ Frontend/lysis/src/Screens/Dashboard/Manager/ManagerMainContent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import avatarImage from "../../../Images/avatar profile.jpg"
 
 const ManagerMainContent = () => {
   const user = {
@@ -7,9 +8,7 @@ const ManagerMainContent = () => {
     contact: "+1234567890",
   };
 
-  const handleUpdateButtonClick = () => {
-    console.log("Update button clicked");
-  };
+
 
   return (
     <div className="col-lg-9 col-md-8 col-sm-7">
@@ -20,25 +19,17 @@ const ManagerMainContent = () => {
               <div className="card mb-4">
                 <div className="card-body text-center">
                   <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                    src={avatarImage}
                     alt="avatar"
                     className="rounded-circle img-fluid"
-                    style={{ width: "150px" }}
+                    style={{ width: "180px" }}
                   />
 
                   <h5 className="my-3">{user.Name}</h5>
                   <p className="text-muted mb-1">{user.email}</p>
-                  <p className="text-muted mb-4">{user.contact}</p>
 
-                  <div className="d-flex justify-content-center">
-                    <button
-                      type="button"
-                      className="btn btn-success"
-                      onClick={handleUpdateButtonClick}
-                    >
-                      Update
-                    </button>
-                  </div>
+
+                
                 </div>
               </div>
             </div>
